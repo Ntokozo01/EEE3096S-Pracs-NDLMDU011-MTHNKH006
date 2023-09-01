41e5876e0ae7b87ef2d57d065376fc2b0028897d
Mduduzi Ntokozo Ndlovu <112991695+Ntokozo01@users.noreply.github.com>
2023-09-01 13:59:28 +0200

    Update main.c

--- Prac_2/main.c
+++ Prac_2/main.c
@@ -1,20 +1,20 @@
 /* USER CODE BEGIN Header */
 /**
-  ******************************************************************************
-  * @file           : main.c
-  * @brief          : Main program body
-  ******************************************************************************
-  * @attention
-  *
-  * Copyright (c) 2023 STMicroelectronics.
-  * All rights reserved.
-  *
-  * This software is licensed under terms that can be found in the LICENSE file
-  * in the root directory of this software component.
-  * If no LICENSE file comes with this software, it is provided AS-IS.
-  *
-  ******************************************************************************
-  */
+ ******************************************************************************
+ * @file           : main.c
+ * @brief          : Main program body
+ ******************************************************************************
+ * @attention
+ *
+ * Copyright (c) 2023 STMicroelectronics.
+ * All rights reserved.
+ *
+ * This software is licensed under terms that can be found in the LICENSE file
+ * in the root directory of this software component.
+ * If no LICENSE file comes with this software, it is provided AS-IS.
+ *
+ ******************************************************************************
+ */
 /* USER CODE END Header */
 /* Includes ------------------------------------------------------------------*/
 #include "main.h"
@@ -34,7 +34,7 @@
 /* USER CODE BEGIN PD */
 
 // Definitions for SPI usage
-#define MEM_SIZE 8192 // bytes
+#define MEM_SIZE 8192   // bytes
 #define WREN 0b00000110 // enable writing
 #define WRDI 0b00000100 // disable writing
 #define RDSR 0b00000101 // read status register
@@ -53,9 +53,9 @@ TIM_HandleTypeDef htim16;
 
 /* USER CODE BEGIN PV */
 // TODO: Define any input variables
-static uint8_t patterns[6] = {0b10101010, 0b01010101,0b11001100,0b00110011,0b11110000,0b00001111};
-volatile uint16_t nextBinary;
-volatile uint8_t isOneSec= 0;
+static uint8_t patterns[6] = {0b10101010, 0b01010101, 0b11001100, 0b00110011, 0b11110000, 0b00001111};
+volatile uint16_t nextBinary;  // index for the next value to display
+volatile uint8_t isOneSec = 0; // boolean variable which stores the value 'is the delay one?'
 
 /* USER CODE END PV */
 
@@ -78,9 +78,9 @@ static void delay(uint32_t delay_in_us);
 /* USER CODE END 0 */
 
 /**
-  * @brief  The application entry point.
-  * @retval int
-  */
+ * @brief  The application entry point.
+ * @retval int
+ */
 int main(void)
 {
   /* USER CODE BEGIN 1 */
@@ -107,14 +107,14 @@ int main(void)
   /* USER CODE BEGIN 2 */
 
   // TODO: Start timer TIM16
-  HAL_TIM_Base_Start_IT(&htim16) ;
-  nextBinary = 0;
+  HAL_TIM_Base_Start_IT(&htim16);
+  nextBinary = 0; // initialise to start with first pattern
 
   // TODO: Write all "patterns" to EEPROM using SPI
-    for (uint16_t i = 0; i < 6; ++i) {
-          write_to_address(i, patterns[i]);
-      }
-
+  for (uint16_t i = 0; i < 6; ++i)
+  {
+    write_to_address(i, patterns[i]);
+  }
 
   /* USER CODE END 2 */
 
@@ -126,60 +126,66 @@ int main(void)
 
     /* USER CODE BEGIN 3 */
 
-	// TODO: Check button PA0; if pressed, change timer delay
-  while ((GPIOA->IDR & GPIO_IDR_0) != 0);
-  if (isOneSec){
-    TIM16->ARR <<= 1;
-    isOneSec = 0;
-  } else {
-    TIM16->ARR >>= 1;
-    isOneSec = 1;
+    // TODO: Check button PA0; if pressed, change timer delay
+    
+    //Wait for the button PA0 to be pressed
+    while ((GPIOA->IDR & GPIO_IDR_0) != 0)
+      ;
+    // Check if the delay is 1, if 1 make it half 1 (divide by 2) else make it 1 (multiply half by 2)
+    // then change the variable of whether its now 1 or not
+    if (isOneSec)
+    {
+      TIM16->ARR <<= 1;
+      isOneSec = 0;
+    }
+    else
+    {
+      TIM16->ARR >>= 1;
+      isOneSec = 1;
+    }
   }
-}
   /* USER CODE END 3 */
 }
 
 /**
-  * @brief System Clock Configuration
-  * @retval None
-  */
+ * @brief System Clock Configuration
+ * @retval None
+ */
 void SystemClock_Config(void)
 {
   LL_FLASH_SetLatency(LL_FLASH_LATENCY_0);
-  while(LL_FLASH_GetLatency() != LL_FLASH_LATENCY_0)
+  while (LL_FLASH_GetLatency() != LL_FLASH_LATENCY_0)
   {
   }
   LL_RCC_HSI_Enable();
 
-   /* Wait till HSI is ready */
-  while(LL_RCC_HSI_IsReady() != 1)
+  /* Wait till HSI is ready */
+  while (LL_RCC_HSI_IsReady() != 1)
   {
-
   }
   LL_RCC_HSI_SetCalibTrimming(16);
   LL_RCC_SetAHBPrescaler(LL_RCC_SYSCLK_DIV_1);
   LL_RCC_SetAPB1Prescaler(LL_RCC_APB1_DIV_1);
   LL_RCC_SetSysClkSource(LL_RCC_SYS_CLKSOURCE_HSI);
 
-   /* Wait till System clock is ready */
-  while(LL_RCC_GetSysClkSource() != LL_RCC_SYS_CLKSOURCE_STATUS_HSI)
+  /* Wait till System clock is ready */
+  while (LL_RCC_GetSysClkSource() != LL_RCC_SYS_CLKSOURCE_STATUS_HSI)
   {
-
   }
   LL_SetSystemCoreClock(8000000);
 
-   /* Update the time base */
-  if (HAL_InitTick (TICK_INT_PRIORITY) != HAL_OK)
+  /* Update the time base */
+  if (HAL_InitTick(TICK_INT_PRIORITY) != HAL_OK)
   {
     Error_Handler();
   }
 }
 
 /**
-  * @brief TIM16 Initialization Function
-  * @param None
-  * @retval None
-  */
+ * @brief TIM16 Initialization Function
+ * @param None
+ * @retval None
+ */
 static void MX_TIM16_Init(void)
 {
 
@@ -191,9 +197,9 @@ static void MX_TIM16_Init(void)
 
   /* USER CODE END TIM16_Init 1 */
   htim16.Instance = TIM16;
-  htim16.Init.Prescaler = 8000-1;
+  htim16.Init.Prescaler = 8000 - 1;
   htim16.Init.CounterMode = TIM_COUNTERMODE_UP;
-  htim16.Init.Period = 1000-1;
+  htim16.Init.Period = 1000 - 1;
   htim16.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
   htim16.Init.RepetitionCounter = 0;
   htim16.Init.AutoReloadPreload = TIM_AUTORELOAD_PRELOAD_ENABLE;
@@ -204,20 +210,19 @@ static void MX_TIM16_Init(void)
   /* USER CODE BEGIN TIM16_Init 2 */
   NVIC_EnableIRQ(TIM16_IRQn);
   /* USER CODE END TIM16_Init 2 */
-
 }
 
 /**
-  * @brief GPIO Initialization Function
-  * @param None
-  * @retval None
-  */
+ * @brief GPIO Initialization Function
+ * @param None
+ * @retval None
+ */
 static void MX_GPIO_Init(void)
 {
   LL_EXTI_InitTypeDef EXTI_InitStruct = {0};
   LL_GPIO_InitTypeDef GPIO_InitStruct = {0};
-/* USER CODE BEGIN MX_GPIO_Init_1 */
-/* USER CODE END MX_GPIO_Init_1 */
+  /* USER CODE BEGIN MX_GPIO_Init_1 */
+  /* USER CODE END MX_GPIO_Init_1 */
 
   /* GPIO Ports Clock Enable */
   LL_AHB1_GRP1_EnableClock(LL_AHB1_GRP1_PERIPH_GPIOF);
@@ -328,140 +333,155 @@ static void MX_GPIO_Init(void)
   GPIO_InitStruct.Pull = LL_GPIO_PULL_NO;
   LL_GPIO_Init(LED7_GPIO_Port, &GPIO_InitStruct);
 
-/* USER CODE BEGIN MX_GPIO_Init_2 */
-/* USER CODE END MX_GPIO_Init_2 */
+  /* USER CODE BEGIN MX_GPIO_Init_2 */
+  /* USER CODE END MX_GPIO_Init_2 */
 }
 
 /* USER CODE BEGIN 4 */
 
 // Initialise SPI
-static void init_spi(void) {
+static void init_spi(void)
+{
 
   // Clock to PB
-  RCC->AHBENR |= RCC_AHBENR_GPIOBEN; 	// Enable clock for SPI port
+  RCC->AHBENR |= RCC_AHBENR_GPIOBEN; // Enable clock for SPI port
 
   // Set pin modes
   GPIOB->MODER |= GPIO_MODER_MODER13_1; // Set pin SCK (PB13) to Alternate Function
   GPIOB->MODER |= GPIO_MODER_MODER14_1; // Set pin MISO (PB14) to Alternate Function
   GPIOB->MODER |= GPIO_MODER_MODER15_1; // Set pin MOSI (PB15) to Alternate Function
   GPIOB->MODER |= GPIO_MODER_MODER12_0; // Set pin CS (PB12) to output push-pull
-  GPIOB->BSRR |= GPIO_BSRR_BS_12; 		// Pull CS high
+  GPIOB->BSRR |= GPIO_BSRR_BS_12;       // Pull CS high
 
   // Clock enable to SPI
   RCC->APB1ENR |= RCC_APB1ENR_SPI2EN;
-  SPI2->CR1 |= SPI_CR1_BIDIOE; 									// Enable output
-  SPI2->CR1 |= (SPI_CR1_BR_0 |  SPI_CR1_BR_1); 					// Set Baud to fpclk / 16
-  SPI2->CR1 |= SPI_CR1_MSTR; 									// Set to master mode
-  SPI2->CR2 |= SPI_CR2_FRXTH; 									// Set RX threshold to be 8 bits
-  SPI2->CR2 |= SPI_CR2_SSOE; 									// Enable slave output to work in master mode
-  SPI2->CR2 |= (SPI_CR2_DS_0 | SPI_CR2_DS_1 | SPI_CR2_DS_2); 	// Set to 8-bit mode
-  SPI2->CR1 |= SPI_CR1_SPE; 									// Enable the SPI peripheral
+  SPI2->CR1 |= SPI_CR1_BIDIOE;                               // Enable output
+  SPI2->CR1 |= (SPI_CR1_BR_0 | SPI_CR1_BR_1);                // Set Baud to fpclk / 16
+  SPI2->CR1 |= SPI_CR1_MSTR;                                 // Set to master mode
+  SPI2->CR2 |= SPI_CR2_FRXTH;                                // Set RX threshold to be 8 bits
+  SPI2->CR2 |= SPI_CR2_SSOE;                                 // Enable slave output to work in master mode
+  SPI2->CR2 |= (SPI_CR2_DS_0 | SPI_CR2_DS_1 | SPI_CR2_DS_2); // Set to 8-bit mode
+  SPI2->CR1 |= SPI_CR1_SPE;                                  // Enable the SPI peripheral
 }
 
 // Implements a delay in microseconds
-static void delay(uint32_t delay_in_us) {
+static void delay(uint32_t delay_in_us)
+{
   volatile uint32_t counter = 0;
   delay_in_us *= 3;
-  for(; counter < delay_in_us; counter++) {
+  for (; counter < delay_in_us; counter++)
+  {
     __asm("nop");
     __asm("nop");
   }
 }
 
 // Write to EEPROM address using SPI
-static void write_to_address(uint16_t address, uint8_t data) {
+static void write_to_address(uint16_t address, uint8_t data)
+{
 
-	uint8_t dummy; // Junk from the DR
+  uint8_t dummy; // Junk from the DR
 
-	// Set the Write Enable latch
-	GPIOB->BSRR |= GPIO_BSRR_BR_12; // Pull CS low
-	delay(1);
-	*((uint8_t*)(&SPI2->DR)) = WREN;
-	while ((SPI2->SR & SPI_SR_RXNE) == 0); // Hang while RX is empty
-	dummy = SPI2->DR;
-	GPIOB->BSRR |= GPIO_BSRR_BS_12; // Pull CS high
-	delay(5000);
+  // Set the Write Enable latch
+  GPIOB->BSRR |= GPIO_BSRR_BR_12; // Pull CS low
+  delay(1);
+  *((uint8_t *)(&SPI2->DR)) = WREN;
+  while ((SPI2->SR & SPI_SR_RXNE) == 0)
+    ; // Hang while RX is empty
+  dummy = SPI2->DR;
+  GPIOB->BSRR |= GPIO_BSRR_BS_12; // Pull CS high
+  delay(5000);
 
-	// Send write instruction
-	GPIOB->BSRR |= GPIO_BSRR_BR_12; 			// Pull CS low
-	delay(1);
-	*((uint8_t*)(&SPI2->DR)) = WRITE;
-	while ((SPI2->SR & SPI_SR_RXNE) == 0); 		// Hang while RX is empty
-	dummy = SPI2->DR;
+  // Send write instruction
+  GPIOB->BSRR |= GPIO_BSRR_BR_12; // Pull CS low
+  delay(1);
+  *((uint8_t *)(&SPI2->DR)) = WRITE;
+  while ((SPI2->SR & SPI_SR_RXNE) == 0)
+    ; // Hang while RX is empty
+  dummy = SPI2->DR;
 
-	// Send 16-bit address
-	*((uint8_t*)(&SPI2->DR)) = (address >> 8); 	// Address MSB
-	while ((SPI2->SR & SPI_SR_RXNE) == 0); 		// Hang while RX is empty
-	dummy = SPI2->DR;
-	*((uint8_t*)(&SPI2->DR)) = (address); 		// Address LSB
-	while ((SPI2->SR & SPI_SR_RXNE) == 0); 		// Hang while RX is empty
-	dummy = SPI2->DR;
+  // Send 16-bit address
+  *((uint8_t *)(&SPI2->DR)) = (address >> 8); // Address MSB
+  while ((SPI2->SR & SPI_SR_RXNE) == 0)
+    ; // Hang while RX is empty
+  dummy = SPI2->DR;
+  *((uint8_t *)(&SPI2->DR)) = (address); // Address LSB
+  while ((SPI2->SR & SPI_SR_RXNE) == 0)
+    ; // Hang while RX is empty
+  dummy = SPI2->DR;
 
-	// Send the data
-	*((uint8_t*)(&SPI2->DR)) = data;
-	while ((SPI2->SR & SPI_SR_RXNE) == 0); // Hang while RX is empty
-	dummy = SPI2->DR;
-	GPIOB->BSRR |= GPIO_BSRR_BS_12; // Pull CS high
-	delay(5000);
+  // Send the data
+  *((uint8_t *)(&SPI2->DR)) = data;
+  while ((SPI2->SR & SPI_SR_RXNE) == 0)
+    ; // Hang while RX is empty
+  dummy = SPI2->DR;
+  GPIOB->BSRR |= GPIO_BSRR_BS_12; // Pull CS high
+  delay(5000);
 }
 
 // Read from EEPROM address using SPI
-static uint8_t read_from_address(uint16_t address) {
+static uint8_t read_from_address(uint16_t address)
+{
 
-	uint8_t dummy; // Junk from the DR
+  uint8_t dummy; // Junk from the DR
 
-	// Send the read instruction
-	GPIOB->BSRR |= GPIO_BSRR_BR_12; 			// Pull CS low
-	delay(1);
-	*((uint8_t*)(&SPI2->DR)) = READ;
-	while ((SPI2->SR & SPI_SR_RXNE) == 0); 		// Hang while RX is empty
-	dummy = SPI2->DR;
+  // Send the read instruction
+  GPIOB->BSRR |= GPIO_BSRR_BR_12; // Pull CS low
+  delay(1);
+  *((uint8_t *)(&SPI2->DR)) = READ;
+  while ((SPI2->SR & SPI_SR_RXNE) == 0)
+    ; // Hang while RX is empty
+  dummy = SPI2->DR;
 
-	// Send 16-bit address
-	*((uint8_t*)(&SPI2->DR)) = (address >> 8); 	// Address MSB
-	while ((SPI2->SR & SPI_SR_RXNE) == 0);		// Hang while RX is empty
-	dummy = SPI2->DR;
-	*((uint8_t*)(&SPI2->DR)) = (address); 		// Address LSB
-	while ((SPI2->SR & SPI_SR_RXNE) == 0); 		// Hang while RX is empty
-	dummy = SPI2->DR;
+  // Send 16-bit address
+  *((uint8_t *)(&SPI2->DR)) = (address >> 8); // Address MSB
+  while ((SPI2->SR & SPI_SR_RXNE) == 0)
+    ; // Hang while RX is empty
+  dummy = SPI2->DR;
+  *((uint8_t *)(&SPI2->DR)) = (address); // Address LSB
+  while ((SPI2->SR & SPI_SR_RXNE) == 0)
+    ; // Hang while RX is empty
+  dummy = SPI2->DR;
 
-	// Clock in the data
-	*((uint8_t*)(&SPI2->DR)) = 0x42; 			    // Clock out some junk data
-	while ((SPI2->SR & SPI_SR_RXNE) == 0); 		// Hang while RX is empty
-	dummy = SPI2->DR;
-	GPIOB->BSRR |= GPIO_BSRR_BS_12; 			    // Pull CS high
-	delay(5000);
+  // Clock in the data
+  *((uint8_t *)(&SPI2->DR)) = 0x42; // Clock out some junk data
+  while ((SPI2->SR & SPI_SR_RXNE) == 0)
+    ; // Hang while RX is empty
+  dummy = SPI2->DR;
+  GPIOB->BSRR |= GPIO_BSRR_BS_12; // Pull CS high
+  delay(5000);
 
-	return dummy;								              // Return read data
+  return dummy; // Return read data
 }
 
 // Timer rolled over
 void TIM16_IRQHandler(void)
 {
-	// Acknowledge interrupt
-	HAL_TIM_IRQHandler(&htim16);
+  // Acknowledge interrupt
+  HAL_TIM_IRQHandler(&htim16);
 
-	// TODO: Change to next LED pattern; output 0x01 if the read SPI data is incorrect
+  // TODO: Change to next LED pattern; output 0x01 if the read SPI data is incorrect
   uint8_t binaryPattern = read_from_address(nextBinary);
 
-  if (binaryPattern != patterns[nextBinary]){
+  if (binaryPattern != patterns[nextBinary])
+  {
     binaryPattern = 0x01;
   }
   GPIOB->ODR = binaryPattern;
   nextBinary++;
 
-  if (nextBinary >= 5) {
+  if (nextBinary >= 6)
+  {
     nextBinary = 0;
   }
-
 }
 
 /* USER CODE END 4 */
 
 /**
-  * @brief  This function is executed in case of error occurrence.
-  * @retval None
-  */
+ * @brief  This function is executed in case of error occurrence.
+ * @retval None
+ */
 void Error_Handler(void)
 {
   /* USER CODE BEGIN Error_Handler_Debug */
@@ -473,14 +493,14 @@ void Error_Handler(void)
   /* USER CODE END Error_Handler_Debug */
 }
 
-#ifdef  USE_FULL_ASSERT
+#ifdef USE_FULL_ASSERT
 /**
-  * @brief  Reports the name of the source file and the source line number
-  *         where the assert_param error has occurred.
-  * @param  file: pointer to the source file name
-  * @param  line: assert_param error line source number
-  * @retval None
-  */
+ * @brief  Reports the name of the source file and the source line number
+ *         where the assert_param error has occurred.
+ * @param  file: pointer to the source file name
+ * @param  line: assert_param error line source number
+ * @retval None
+ */
 void assert_failed(uint8_t *file, uint32_t line)
 {
   /* USER CODE BEGIN 6 */
